6bd2ac8f5d53d522347be3fa213abf4855abbef8
Zakhar Chervyakov <zaharalone@gmail.com>
2023-01-16 11:58:10 +0400

    add tween system

---
+++ Assets/Dev/Tools/Tween/Delay/DelayData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GameSystem.Delay
+{
+    public struct DelayData
+    {
+        public Action delayedFun;
+        public Func<bool> validate;
+    }
+}
---
+++ Assets/Dev/Tools/Tween/Tweens/TweenData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameSystem.Tweens
+{
+    public struct TweenData
+    {
+        public float remain;
+        public Action<float> update;
+        public Action onEnd;
+        public Func<bool> validate;
+    }
+}
